a241140e55f7a9af35f81ba0090352df22bd28cf
Adir Digmi <morisos06@gmail.com>
2021-05-07 20:20:36 +0300

    Rewrite game_logic.py

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,8 +1,96 @@
-import src.player
 import src.deck
+import src.player
+from enum import IntEnum
 
 
-# TODO : comparison between two players if they returned the same thing
+class Match(IntEnum):
+
+    High_card = 1
+    Pair = 2
+    Two_pair = 3
+    Three_of_a_kind = 4
+    Straight = 5
+    Flush = 6
+    Full_house = 7
+    Four_of_a_kind = 8
+    Straight_flash = 9
+    Royal_flush = 10
+
+
+def same_card_counter(cards):
+    counts = {i: [] for i in range(1, 15)}
+    for card in cards:
+        counts[card.card_value].append(card)
+    return counts
+
+
+def rank_type_counter(cards):
+    counts = same_card_counter(cards)
+    first_max, second_max = significant_cards(counts)
+    kickers = list(set(cards) - set(counts[first_max]) - set(counts[second_max]))
+    kickers.sort(reverse=True)
+    final_cards = counts[first_max] + counts[second_max] + kickers
+    final_cards = final_cards[:5]
+    rank = rank_counter(len(counts[first_max]), len(counts[second_max]))
+    return final_cards, rank
+
+
+def rank_decider(player, board):
+    cards = player.hand + board
+    flush_hand, flush_rank = flush(cards)
+    if flush_rank in [Match.Straight_flash, Match.Royal_flush]:
+        return flush_hand, flush_rank
+    counter_hand, counter_rank = rank_type_counter(cards)
+    if counter_rank in [Match.Four_of_a_kind, Match.Full_house]:
+        return counter_hand, counter_rank
+    if flush_rank in [Match.Flush]:
+        return flush_hand, flush_rank
+    straight_hand, straight_rank = straight(cards)
+    if straight_rank in [Match.Straight]:
+        return straight_hand, straight_rank
+    return counter_hand, counter_rank
+
+
+def significant_cards(counts):
+    first_max = 1
+    second_max = 1
+    for i in counts.keys():
+        if len(counts[i]) >= len(counts[first_max]):
+            second_max = first_max
+            first_max = i
+        elif len(counts[i]) >= len(counts[second_max]):
+            second_max = i
+    return first_max, second_max
+
+
+def rank_counter(first_max_len, second_max_len):
+    if first_max_len == 1:
+        return Match.High_card
+    if first_max_len == 2:
+        if second_max_len == 2:
+            return Match.Two_pair
+        return Match.Pair
+    if first_max_len == 3:
+        if second_max_len == 2:
+            return Match.Full_house
+        return Match.Three_of_a_kind
+    if first_max_len == 4:
+        return Match.Four_of_a_kind
+
+
+def is_straight(cards):
+    for i in range(4):
+        if cards[i].card_value - cards[i + 1].card_value != 1:
+            return False
+    return True
+
+
+def straight(cards):
+    cards.sort(reverse=True)
+    for i in range(len(cards) - 5):
+        if is_straight(cards[i:i + 5]):
+            return cards[i - 5:i], Match.Straight
+    return None, None
 
 
 def flush(cards):
@@ -12,129 +100,25 @@ def flush(cards):
         sorter[card.suit_type].append(card)
     for suit_arr in sorter.values():
         if len(suit_arr) >= 5:
-            suit_arr.sort()
-            return suit_arr[-5:]
-    return None
-
-
-def straight(cards):
-    cards.sort()
-    for i in range(len(cards), 4, -1):
-        if is_straight(cards[i - 5:i]):
-            return cards[i - 5:i]
-    return None
-
-
-def is_straight(cards):
-    for i in range(4):
-        if cards[i + 1].card_value - cards[i].card_value != 1:
-            return False
-    return True
-
-
-def card_counter(cards):
-    counts = same_card_counter(cards)
-    max, second_max = max_list_in_dict(counts)
-    # FOUR OF A KIND
-    if len(counts[max]) == 4:
-        return counts[max]
-    # FULL HOUSE
-    if len(counts[max] == 3) and len(counts[second_max] >= 2):
-        return counts[max] + counts[second_max][:2]
-    # THREE OF A KIND
-    if len(counts[max] == 3):
-        return counts[max]
-    # TWO PAIRS
-    if len(counts[max] == 2) and len(counts[second_max] == 2):
-        return counts[max], counts[second_max]
-    # PAIR
-    if len(counts[max] == 2):
-        return counts[max]
-    # HIGH CARD
-    if len(counts[max] == 1):
-        return counts[max]
-
-
-def same_card_counter(cards):
-    counts = {i: [] for i in range(2, 15)}
-    for card in cards:
-        counts[card.card_value].append(card)
-    return counts
-
-
-def max_list_in_dict(cards_dict):
-    max = 2
-    second_max = 2
-    for i in cards_dict.keys():
-        if len(cards_dict[i]) >= len(cards_dict[max]):
-            max = i
-        elif len(cards_dict[i]) >= len(cards_dict[second_max]):
-            second_max = i
-
-    return max, second_max
-
-
-def compare_flush(player1, player2, board):
-    player1_total = player1.hand + board
-    player2_total = player2.hand + board
-
-    player1_flush = flush(player1_total)
-    if player1_flush is not None:
-
-        player1_flush = set(player1_flush)
-    player2_flush = flush(player2_total)
-    if player2_flush is not None:
-        player2_flush = set(player2_flush)
-    if player1_flush is not None and player2_flush is not None:
-        if len(player1_flush.difference(player2_flush)) == 0:
-            print(f"Draw between {player1.name} and {player2.name} with the flash : {board}")
-            return [player1, player2]
-        p1_selected_card = max(player1_flush.difference(player2_flush))
-        p2_selected_card = max(player2_flush.difference(player1_flush))
-        if p1_selected_card > p2_selected_card:
-            print(f"{player1.name} wins with flash : {player1_flush}")
-            return [player1]
-        print(f"{player2.name} wins with flash : {player2_flush}")
-        return [player2]
-
-    if player1_flush is not None:
-        print(f"{player1.name} wins with flash : {player1_flush}")
-        return [player1]
-    if player2_flush is not None:
-        print(f"{player2.name} wins with flash : {player2_flush}")
-        return [player2]
-    return None
-
-
-def compare_straight(player1, player2, board):
-    player1_total = player1.hand + board
-    player2_total = player2.hand + board
-
-    player1_straight = straight(player1_total)
-    player2_straight = straight(player2_total)
-    if player1_straight is not None and player2_straight is not None:
-        if player1_straight[0].card_value == player2_straight[0].card_value:
-            print(
-                f"Draw between {player1.name} and {player2.name}\n {player1.name} straight is : {player1_straight}\n {player2.name} straight is : {player2_straight} ")
-            return [player1, player2]
-        if player1_straight[0] > player2_straight[0]:
-            print(f"{player1.name} wins with straight : {player1_straight}")
-            return [player1]
-        print(f"{player2.name} wins with straight : {player2_straight}")
-        return [player2]
-    if player1_straight is not None:
-        print(f"{player1.name} wins with straight : {player1_straight}")
-        return [player1]
-    if player2_straight is not None:
-        print(f"{player2.name} wins with straight : {player2_straight}")
-        return [player2]
-    return None
+            straight_flush, _ = straight(suit_arr)
+            if straight_flush is not None:
+                if straight_flush[0].card_value == 10:
+                    return straight_flush, Match.Royal_flush
+                return straight_flush, Match.Straight_flash
+            return suit_arr[:5], Match.Flush
+    return None, None
 
 
 def compare_hands(player1, player2, board):
-    res = compare_flush(player1, player2, board)
-    if res is not None:
-        return res
-    res = compare_straight(player1, player2, board)
-    if res is not None:
-        return res
+    player1_hand, player1_rank = rank_decider(player1, board)
+    player2_hand, player2_rank = rank_decider(player2, board)
+    if player1_rank > player2_rank:
+        return [player1], player1_rank, [player1_hand]
+    if player1_rank < player2_rank:
+        return [player2], player2_rank, [player2_hand]
+    for card1, card2 in zip(player1_hand, player2_hand):
+        if card1 > card2:
+            return [player1], player1_rank, [player1_hand]
+        if card1 < card2:
+            return [player2], player2_rank, [player2_hand]
+    return [player1, player2], player1_rank, [player1_hand, player2_hand]
--- src/main.py
+++ src/main.py
@@ -5,29 +5,34 @@ from game_logic import *
 
 
 def main():
-
-    #d = d.Deck()
-    p1 = p.player(3000,"Adir")
+    # d = d.Deck()
+    p1 = p.player(3000, "Adir")
     p2 = p.player(3000, "Tomer")
-    p1.add_card(c.Card(9, "Clubs"))
-    p1.add_card(c.Card(3, "Clubs"))
-    p2.add_card(c.Card(8, "Diamonds"))
-    p2.add_card(c.Card(9, "Clubs"))
-    board = [c.Card(10, "Clubs"), c.Card(11, "Clubs"), c.Card(12, "Clubs"), c.Card(14, "Diamonds"), c.Card(5, "Hearts")]
+    p1.add_card(c.Card(11, "Hearts"))
+    p1.add_card(c.Card(13, "Clubs"))
+    p2.add_card(c.Card(9, "Spades"))
+    p2.add_card(c.Card(5, "Spades"))
+    board = [c.Card(9, "Hearts"), c.Card(3, "Spades"), c.Card(6, "Spades"), c.Card(10, "Spades"),
+             c.Card(12, "Spades")]
     print(p1)
     print(p2)
     print(f"The board is : {board}")
-    compare_hands(p1, p2, board)
+    winners, rank, hands = compare_hands(p1, p2, board)
+    if len(winners) >= 2:
+        print(f"Its a draw between {[player.name for player in winners]} , with rank : {rank} ",
+              [player.name + 'hand :' + hands for player, hand in zip(winners, hands)])
+    else:
+        print(f"The winner is {winners[0].name} with rank : {rank.name}, {winners[0].name} hand : {hands[0]} ")
 
     # for _ in range(3):
     #     P.get_card(D.draw_card())
-    #D.cards.sort()
-    #print(D)
+    # D.cards.sort()
+    # print(D)
     # C = D.draw_card()
     # print("______________")
     # print(C.suit + C.value + ".png")
-    #list1.sort()
-    #print(list1[-5:])
+    # list1.sort()
+    # print(list1[-5:])
 
 
 if __name__ == '__main__':
